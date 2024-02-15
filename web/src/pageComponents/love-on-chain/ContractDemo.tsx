06c827714ee069f2746581e7c3b74e80fb9ed714
Anika Hamby <anika.hamby01@gmail.com>
2024-02-15 13:23:05 -0800

    final product

--- web/src/pageComponents/love-on-chain/ContractDemo.tsx
+++ web/src/pageComponents/love-on-chain/ContractDemo.tsx
@@ -1,23 +1,38 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { clsx } from 'clsx';
 import AccountConnect from '../../components/header/AccountConnect';
+import { useLoveOnChainContract } from '../../hooks/contracts';
 import { INVITES } from '../../mocks/userMocks';
 import { Invite } from '../../types';
 import MatchProfile from '../love-on-chain/MatchProfile';
 import Profile from '../love-on-chain/Profile';
 import DateConfirmation from './DateConfirmation';
-
+import { writeContract } from '@wagmi/core'
+import {wagmiConfig} from '../../providers/OnchainProviders';
 export enum TransactionSteps {
   START_TRANSACTION_STEP,
   TRANSACTION_COMPLETE_STEP,
   OUT_OF_GAS_STEP,
+  CONFIRM_ATTENDANCE_STEP,
 }
 
 export default function LoveOnChainContractDemo() {
   const [selectedInvite, setSelectedInvite] = useState<Invite>();
-  
-  const onSelectMatch = useCallback((invite: Invite) => {
+  const contract = useLoveOnChainContract();
+  const onSelectMatch = useCallback( async (invite: Invite) => {
     setSelectedInvite(invite);
+
+    await writeContract(wagmiConfig, {
+      address: '0x5271F6dfE8080c1dc6E110E83D8687b54fAf1f9c',
+      abi: contract.abi,
+      functionName: 'initDate',
+      args: [
+          'anika-ana',
+          '0x4046aF2e421651CFd6080B85A96d200be91C676B',
+          '0xBa6618c6E109cA31F7E22e80557117f9813D5b49'
+      ]
+    });
+
     
   }, [setSelectedInvite])
 
--- web/src/pageComponents/love-on-chain/steps/LoveOnChainFormStep/FormCommitToDate.tsx
+++ web/src/pageComponents/love-on-chain/steps/LoveOnChainFormStep/FormCommitToDate.tsx
@@ -70,57 +70,64 @@ function FormCommitToDate({
     },
   });
 
+  let txSuccess = false;
+
   useEffect(() => {
-    async function handleTransactionStatus() {
-      if (transactionStatus === 'error' && dataHash !== '') {
-        await handleOncomplete();
-        if (
-          errorLoveOnChain instanceof TransactionExecutionError &&
-          errorLoveOnChain.message.toLowerCase().includes('out of gas')
-        ) {
-          setTransactionStep(TransactionSteps.OUT_OF_GAS_STEP);
-        } else {
-          setTransactionStep(null);
-        }
-      } else if (transactionStatus === 'success' && dataHash !== '') {
-        await handleOncomplete();
-        setDataHash('');
+      if (txSuccess) {
         setTransactionStep(TransactionSteps.TRANSACTION_COMPLETE_STEP);
+        console.log(transactionStep);
       }
-    }
-    void handleTransactionStatus();
-  }, [
-    dataHash,
-    errorLoveOnChain,
-    handleOncomplete,
-    setTransactionStep,
-    transactionStatus,
-  ]);
+  }, [txSuccess, setTransactionStep, transactionStep]);
 
-
-  const handleSubmit = useCallback(
+  const confirmAttendance = useCallback(
     async (event: { preventDefault: () => void }) => {
       event.preventDefault();
         console.log('Commiting');
-        await writeContract(wagmiConfig, {
-          address: '0x5271F6dfE8080c1dc6E110E83D8687b54fAf1f9c',
-          abi: contract.abi,
-          functionName: 'initDate',
-          args: [
-              'anika-ana',
-              '0x4046aF2e421651CFd6080B85A96d200be91C676B',
-              '0xBa6618c6E109cA31F7E22e80557117f9813D5b49'
-          ]
-        });
+          await writeContract(wagmiConfig, {
+            address: '0x5271F6dfE8080c1dc6E110E83D8687b54fAf1f9c',
+            abi: contract.abi,
+            functionName: 'confirmAttendanceInt',
+            args: [
+                'anika-ana',
+                '0x4046aF2e421651CFd6080B85A96d200be91C676B',
+            ],
+          });
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
 
         await writeContract(wagmiConfig, {
           address: '0x5271F6dfE8080c1dc6E110E83D8687b54fAf1f9c',
           abi: contract.abi,
-          functionName: 'stake',
+          functionName: 'confirmAttendanceInt',
           args: [
               'anika-ana',
-          ]
+              '0xBa6618c6E109cA31F7E22e80557117f9813D5b49',
+          ],
         });
+        
+    },
+    [contract.abi],
+  );
+
+  const handleSubmit = useCallback(
+
+    async (event: { preventDefault: () => void }) => {
+
+      txSuccess = false;
+      event.preventDefault();
+        console.log('Commiting');
+          await writeContract(wagmiConfig, {
+            address: '0x5271F6dfE8080c1dc6E110E83D8687b54fAf1f9c',
+            abi: contract.abi,
+            functionName: 'stakeInt',
+            args: [
+                'anika-ana',
+                '0x4046aF2e421651CFd6080B85A96d200be91C676B',
+                BigInt(100),
+            ],
+            value: parseEther(String(MIN_STAKE)),
+          });
+        await new Promise(resolve => setTimeout(resolve, 2000));
 
         await writeContract(wagmiConfig, {
           address: '0x5271F6dfE8080c1dc6E110E83D8687b54fAf1f9c',
@@ -130,10 +137,14 @@ function FormCommitToDate({
               'anika-ana',
               '0xBa6618c6E109cA31F7E22e80557117f9813D5b49',
               BigInt(100),
-          ]
+          ],
+          value: parseEther(String(MIN_STAKE)),
         });
+
+        txSuccess = true
+        
         //loveOnChain?.(loveOnChainData?.request);
-        //setTransactionStep(TransactionSteps.TRANSACTION_COMPLETE_STEP);
+        setTransactionStep(TransactionSteps.TRANSACTION_COMPLETE_STEP);
         //setDataHash(dataLoveOnChain);
 
         //const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({
@@ -183,7 +194,7 @@ function FormCommitToDate({
       {transactionStep === TransactionSteps.START_TRANSACTION_STEP && <StartTransactionStep />}
 
       {transactionStep === TransactionSteps.TRANSACTION_COMPLETE_STEP && (
-        <TransactionCompleteStep stake={MIN_STAKE}/>
+        <TransactionCompleteStep stake={MIN_STAKE} confirmAttendance={confirmAttendance}/>
       )}
 
       {transactionStep === TransactionSteps.OUT_OF_GAS_STEP && (
@@ -195,7 +206,7 @@ function FormCommitToDate({
           <h2 className="mb-5 w-full text-center text-2xl font-semibold text-white lg:text-left">
             Say no to ghosting!
           </h2>
-          <form onSubmit={handleSubmit} className="w-full">
+          <form className="w-full">
               {warningContent ? (
                 <div className="my-3 flex items-center justify-center">
                   <div className="mr-2">
